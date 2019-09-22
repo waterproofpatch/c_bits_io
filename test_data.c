3b1ded77ca4db70cd5a0ec70234ee706cd181141
waterproofpatch <waterproofpatch@gmail.com>
2019-09-22 12:22:31 -0400

    more generic

--- test_data.c
+++ test_data.c
@@ -1,6 +1,7 @@
 #include "test_data.h"
 
 test_entry_t test_entries[] = {
+    {.bits_set = 1, .test_data = 0x00000001},
     {.bits_set = 24, .test_data = 0xdeadbeef},
     {.bits_set = 24, .test_data = 0xfeedface},
     {.bits_set = 16, .test_data = 0x0ff0ff00},
