8d5b9b2a11999e59a58acce7d47da271c1e0e00f
waterproofpatch <waterproofpatch@gmail.com>
2020-02-15 21:41:16 -0500

    comments

--- src/bits_print.h
+++ src/bits_print.h
@@ -7,6 +7,10 @@
 
 #include <stddef.h> /* size_t */
 
+/**
+ * @brief print the bits in each byte
+ * @param data: ptr to data of length 'length' to print
+ */
 void bits_print(void *data, size_t length);
 
 #endif /* _BITS_PRINT_H_ */
