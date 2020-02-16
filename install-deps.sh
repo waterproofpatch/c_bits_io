d385fa1a4bd0353f2610c88255121e4c30df72b3
waterproofpatch <waterproofpatch@gmail.com>
2020-02-15 22:08:53 -0500

    removed commented code

--- install-deps.sh
+++ install-deps.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # for travis-ci
 sudo apt-get update
-# sudo apt-get install python3-pip -y
-# sudo apt-get install doxygen -y
-# sudo apt-get install ruby -y
-# sudo pip3 install gcovr
 sudo apt-get install gcc -y
-# sudo gem install ceedling
