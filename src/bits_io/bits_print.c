f5e0cee7328ada30f7e83d2e5e4a681503493560
waterproofpatch <waterproofpatch@gmail.com>
2020-02-26 22:12:58 -0500

    move static above use

--- src/bits_io/bits_print.c
+++ src/bits_io/bits_print.c
@@ -8,27 +8,10 @@
 #include "bits_print.h" /* prototype for this function */
 
 /**
- * @brief print the bits in a byte
- * @param byte: the byte (assumed to be 8 bits) to print
+ * @brief print the bits in the byte
+ *
+ * @param byte
  */
-static void bits_print_byte(unsigned char byte);
-
-/**
- * @brief print the bits in each byte
- * @param data: ptr to data of length 'length' to print
- */
-void bits_print(void *data, size_t length)
-{
-    size_t byte = 0;
-    for (byte = 0; byte < length; byte++)
-    {
-        char *pByte = (char *)data;
-        bits_print_byte(*pByte);
-        data = (char *)data + 1;
-    }
-    printf("\n");
-}
-
 static void bits_print_byte(unsigned char byte)
 {
     /* for each bit in a byte */
@@ -45,3 +28,18 @@ static void bits_print_byte(unsigned char byte)
     }
 }
 
+/**
+ * @brief print the bits in each byte
+ * @param data: ptr to data of length 'length' to print
+ */
+void bits_print(void *data, size_t length)
+{
+    size_t byte = 0;
+    for (byte = 0; byte < length; byte++)
+    {
+        char *pByte = (char *)data;
+        bits_print_byte(*pByte);
+        data = (char *)data + 1;
+    }
+    printf("\n");
+}
